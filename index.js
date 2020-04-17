cd5fcc0f146cdb78067a0989d897b8468d8be0e9
Øyvind Moldestad <moldestad@gmail.com>
2020-04-17 13:44:13 +0200

    feat: add a11y-plugin

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
+  plugins: ["jsx-a11y"],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
